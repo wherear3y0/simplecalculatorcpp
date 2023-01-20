61423d9359462daa45add3a9224e997b1cfddb44
whereareyo <104694707+wherear3y0@users.noreply.github.com>
2023-01-20 14:03:39 +0300

    init

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/wherear3y0/C++/ClionProjects/snake
+# Install script for directory: /Users/wherear3y0/C++/ClionProjects/simplecalculatorcpp
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/wherear3y0/C++/ClionProjects/snake/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/wherear3y0/C++/ClionProjects/simplecalculatorcpp/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
